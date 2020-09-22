910d5960239b371756f3cb26f92e9a4a19364a00
petson123 <erik.p2000@hotmail.com>
2020-09-22 16:08:13 +0200

    Created GridBuilder class

---
+++ TicTacToe AI/GridBuilder.cpp
@@ -0,0 +1,32 @@
+#include "GridBuilder.h"
+
+void GridBuilder::GenerateGrid()
+{
+	const int32_t gridSize = 3;
+
+	char grid[gridSize][gridSize];
+
+	int number = 1;
+
+	for (int x = 0; x < gridSize; x++)
+	{
+		for (int y = 0; y < gridSize; y++)
+		{
+			grid[x][y] = to_string(number).c_str()[0];
+			number += 1;
+		}
+	}
+
+	printf("\n------------\n");
+
+	for (int x = 0; x < gridSize; x++)
+	{
+
+		for (int y = 0; y < gridSize; y++)
+		{
+			printf(" %c |", grid[x][y]);
+		}
+
+		printf("\n------------\n");
+	}
+}
